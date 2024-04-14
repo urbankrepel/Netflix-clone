c3f28377203840c093c5f937896938139ec2fc17
Deen Memić <deen.memic@scv.si>
2024-04-14 07:30:38 +0200

    update

--- src/app/(protected)/browse/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function BrowsePage() {
-  return (
-    <div>
-      <h1>Browse Page</h1>
-    </div>
-  );
-}
--- src/routes.ts
+++
@@ -1,7 +0,0 @@
-export const publicRoutes = ["/"];
-
-export const authRoutes = ["/auth/login", "/auth/register"];
-
-export const apiAuthPrefix = "/api/auth";
-
-export const DEFAULT_LOGIN_REDIRECT = "/browse";
